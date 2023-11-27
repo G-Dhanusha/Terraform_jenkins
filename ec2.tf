5ce3304c503c34c4c52ed0a4c3e9f6d29b3b5762
G-Dhanusha <118172980+G-Dhanusha@users.noreply.github.com>
2023-11-27 09:19:15 +0530

    Updated ec2

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami                     = "ami-0cbd40f694b804622"
   instance_type           = "t2.micro"
-  key_name                = "project1"
+  key_name                = "docker"
   vpc_security_group_ids  = [aws_security_group.sg.id]
   tags = {
     "Name" = "terraform"
@@ -10,14 +10,14 @@ resource "aws_instance" "web" {
 connection {
       type     = "ssh"
       user     = "ubuntu"
-      private_key = file("project1.pem")  # Update with the path to your private key
+      private_key = file("docker.pem")  # Update with the path to your private key
       host     = aws_instance.web.public_ip
     }
 provisioner "remote-exec" {
     inline = [
       "sudo apt-get update",
-      # "sudo apt-get install nginx -y",
-      # "sudo apt-get install unzip -y",
+      "sudo apt-get install nginx -y",
+      "sudo apt-get install unzip -y",
       "wget https://www.free-css.com/assets/files/free-css-templates/download/page284/pet-shop.zip",
       "sudo unzip pet-shop.zip",
       "sudo mv pet-shop-website-template /var/www/html/"
