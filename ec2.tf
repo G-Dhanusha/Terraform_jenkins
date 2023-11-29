3fb01a2be9faa7369e2818a038be340827e19763
G-Dhanusha <118172980+G-Dhanusha@users.noreply.github.com>
2023-11-29 11:51:00 +0530

    Updated ec2

--- ec2.tf
+++ ec2.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "web" {
-  ami                     = "ami-0cbd40f694b804622"
+  ami                     = "ami-093467ec28ae4fe03"
   instance_type           = "t2.micro"
   key_name                = "docker"
   vpc_security_group_ids  = [aws_security_group.sg.id]
@@ -9,18 +9,18 @@ resource "aws_instance" "web" {
 
 connection {
       type     = "ssh"
-      user     = "ubuntu"
+      user     = "ec2-user"
       private_key = file("docker.pem")  # Update with the path to your private key
       host     = aws_instance.web.public_ip
     }
 provisioner "remote-exec" {
     inline = [
       "sudo yum update",
-      "sudo yum install nginx -y",
+      "sudo yum install httpd -y",
       "sudo yum install unzip -y",
       "wget https://www.free-css.com/assets/files/free-css-templates/download/page284/pet-shop.zip",
       "sudo unzip pet-shop.zip",
-      "sudo mv pet-shop-website-template /var/www/html/"
+      "sudo mv -f pet-shop-website-template /var/www/html/"
     ]
   }
 }
